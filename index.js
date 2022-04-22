b183a6c290706d31fb013aa6a92e25dbcba44ef8
Hoffmann-Ferreira <andreihf@outlook.com>
2022-04-22 20:11:21 -0300

    Adcionado array com objetos de pokemon

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(express.static(path.join(__dirname, "public")))
 const port = 3001
 
 app.listen(port,() => {
-    console.log(`Rodando na porta ${port}`)
+    
 });
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
-        cavaleiros
+        pokedex
     });
 
 });
@@ -26,17 +26,40 @@ app.get("/pokemon", (req, res) =>{
 }); 
 //          A CIMA APENAS CONFIGURAÇÕES GERAIS
 
-let cavaleiros = [
+let pokedex = [
     {
-        nome: 'Aioros',
-        singno: 'Touro'
+        id: 1,
+        nome: "Pikachu",
+        tipo:"Eletric",
+        imagen:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+        descrição:"Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
+        altura:"0.4 m",
+        peso:"6.0 Kg",
+        categoria:"Mouse",
+        habilidade:"Static",
     },
     {
-    nome: 'Aldebaran',
-    singno: 'Escorpião'
+        id: 2,
+        nome: "Bulbasaur",
+        tipo:"Grass",
+        imagen:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        descrição:"There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+        altura:"0.7 m",
+        peso:"6.9 Kg",
+        categoria:"Seed",
+        habilidade:"Overgrow",
     },
     {
-        nome: 'Afrodite',
-        singno: 'Virgen'
-        }
+        id: 3,
+        nome: "Charizard ",
+        tipo:"Fire",
+        imagen:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+        descrição:"It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+        altura:"1.7 m",
+        peso:"90.5 Kg",
+        categoria:"Flame",
+        habilidade:"Blaze",
+    },
 ]
+
+
