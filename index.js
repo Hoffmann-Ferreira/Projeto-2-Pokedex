f69fc39f127712250c2b619f2c58c718a519d2ce
Hoffmann-Ferreira <andreihf@outlook.com>
2022-04-21 22:01:24 -0300

    Instalação dependências Express, Nodemon e ejs

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 import express from "express";
+import path, { dirname } from "path"
+
+const __dirname = path.resolve(path.dirname(""));
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
+
 const port = 3001
 
 app.listen(port,() => {
     console.log(`Rodando na porta ${port}`)
 });
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        cavaleiros
+    });
+
+});
+
+app.get("/pokemon", (req, res) =>{
+    res.send("Eu sou a página do pokemon");
+}); 
+//          A CIMA APENAS CONFIGURAÇÕES GERAIS
+
+let cavaleiros = [
+    {
+        nome: 'Aioros',
+        singno: 'Touro'
+    },
+    {
+    nome: 'Aldebaran',
+    singno: 'Escorpião'
+    },
+    {
+        nome: 'Afrodite',
+        singno: 'Virgen'
+        }
+]
